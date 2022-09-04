e7268c50ec006769eeead839dead3f724aafabbb
erikjseidel <70927748+erikjseidel@users.noreply.github.com>
2022-09-04 17:43:05 -0500

    9/4 update

--- modules/routers.py
+++ modules/routers.py
@@ -4,8 +4,8 @@ import copy
 #  Currently only VyOS routers + API available
 #
 lg_routers = {
-        "lhr": { "address": "192.0.2.1", "type": "VyOS", "description": "London, UK"},
-        "ewr": { "address": "192.0.2.2", "type": "VyOS", "description": "New York, NY"},
+        "dfw": { "address": "23.181.64.227", "v4-source": "23.181.64.227", "v6-source": "2620:136:a009:af00::227", "type": "VyOS", "description": "Dallas, TX"},
+        "mci": { "address": "23.181.64.228", "v4-source": "23.181.64.228", "v6-source": "2620:136:a009:af00::228", "type": "VyOS", "description": "Kansas City, MO"},
 }
 
 #
@@ -14,7 +14,6 @@ lg_routers = {
 lg_routers_pub = copy.deepcopy(lg_routers)
 for key in lg_routers_pub:
     lg_routers_pub[key].pop('address', None)
-
 
 def get_routers():
     return lg_routers
